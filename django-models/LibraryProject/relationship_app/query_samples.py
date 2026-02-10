2dd92d9986f74d209c99fccc0bd28f0960a34e2d
ganiyu lukman <lganiyu43@gmail.com>
2026-02-10 22:36:55 +0100

    Add new update in querry

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,8 @@
-from .models import Book, Library
+from .models import Book, Library, Author, Librarian
 
 def books_in_library(library_id):
     """Return all books in a given library."""
     return Book.objects.filter(library_id=library_id)
-def Author.objects.get(name=author_name)", "objects.filter(author=author)
 
 def books_by_author(author_id):
     """Return all books written by a given author."""
@@ -13,4 +12,20 @@ def librarian_for_library(library_id):
     """Return the librarian responsible for a given library."""
     library = Library.objects.get(id=library_id)
     return library.librarian
-def Library.objects.get(name=library_name)", "books.all()
+
+def get_author_by_name(author_name):
+    """Return an author object by name."""
+    return Author.objects.get(name=author_name)
+
+def get_library_by_name(library_name):
+    """Return a library object by name."""
+    return Library.objects.get(name=library_name)
+
+def books_in_library_by_name(library_name):
+    """Return all books in a library given its name."""
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+def librarian_by_library(library_id):
+    """Return librarian using Librarian.objects.get with library filter."""
+    return Librarian.objects.get(library=library_id)
