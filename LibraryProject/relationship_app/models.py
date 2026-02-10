ea091f9220d09025ae359a76141e444622304f6f
ganiyu lukman <lganiyu43@gmail.com>
2026-02-10 04:31:41 +0100

    Add complete models and query functions

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,3 +1,20 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    library = models.ForeignKey("Library", on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
+
 class Library(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
--- LibraryProject/relationship_app/query_samples.py
+++ LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,14 @@
 from .models import Book, Library
 
 def books_in_library(library_id):
+    """Return all books in a given library."""
     return Book.objects.filter(library_id=library_id)
 
 def books_by_author(author_id):
+    """Return all books written by a given author."""
     return Book.objects.filter(author_id=author_id)
 
 def librarian_for_library(library_id):
+    """Return the librarian responsible for a given library."""
     library = Library.objects.get(id=library_id)
     return library.librarian
