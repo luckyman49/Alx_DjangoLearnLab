a54b952d1a412f876c2572f91daecf7c1de0c5d3
ganiyu lukman <lganiyu43@gmail.com>
2026-02-10 22:23:49 +0100

    Add new update in querry

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,3 +12,4 @@ def librarian_for_library(library_id):
     """Return the librarian responsible for a given library."""
     library = Library.objects.get(id=library_id)
     return library.librarian
+def Library.objects.get(name=library_name)", "books.all()
