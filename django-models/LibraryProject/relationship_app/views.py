2bb15e5c0cef040f72d46bb9990ce4a1c8363ad5
ganiyu lukman <lganiyu43@gmail.com>
2026-02-11 09:12:39 +0100

    Add new update in viwe.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,6 @@ def list_books(request):
 
 # Class-based view: show details for a specific library
 class LibraryDetailView(DetailView):
-    model = relationship_app/library_detail.html",
-    template_name = "library_detail.html"
+    model = Library
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
