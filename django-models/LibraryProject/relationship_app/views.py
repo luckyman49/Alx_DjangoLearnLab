159af45e72b3be2f128653e3a56df4902171cba0
ganiyu lukman <lganiyu43@gmail.com>
2026-02-11 18:51:30 +0100

    I have add the important content to view.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import DetailView
 from .models import Book
