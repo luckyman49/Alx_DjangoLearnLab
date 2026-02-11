03cbc445c7f7f4df5ec6d5751a7edc38cb4a9fa9
ganiyu lukman <lganiyu43@gmail.com>
2026-02-11 09:51:40 +0100

    Add new update in viwe.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,9 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/book_list.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
