b6c87056bc397a99c410550254e9fdb13e09410a
ganiyu lukman <lganiyu43@gmail.com>
2026-02-20 23:48:26 +0100

    Implement Role-Based Access Control in relationship_app

--- LibraryProject/relationship_app/urls.py
+++ LibraryProject/relationship_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin-view/", views.admin_view, name="admin_view"),
-    path("librarian-view/", views.librarian_view, name="librarian_view"),
-    path("member-view/", views.member_view, name="member_view"),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
+
