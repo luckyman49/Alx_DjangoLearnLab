2f88c0745a089f9477e874fc71004b1e6ec578d6
ganiyu lukman <lganiyu43@gmail.com>
2026-02-23 10:37:07 +0100

    Role-based access control with UserProfile, signals, views, urls, and templates

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("relationship_app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
-
