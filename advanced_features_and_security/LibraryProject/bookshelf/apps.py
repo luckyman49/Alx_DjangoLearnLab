7f400992de8015e5531707be77b9c1c60de089b7
ganiyu lukman <lganiyu43@gmail.com>
2026-02-27 00:10:21 +0100

    Fix bookshelf app path and package setup

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class BookshelfConfig(AppConfig):
-    name = 'bookshelf'
+    efault_auto_field = "django.db.models.BigAutoField" 
+    name= "LibraryProject.bookshelf"
