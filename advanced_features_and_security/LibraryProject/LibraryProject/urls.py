e6ee87301868a55ce584670bd81fa8f9c43e2741
ganiyu lukman <lganiyu43@gmail.com>
2026-02-26 03:28:08 +0100

    Remove relationship_app to start fresh with bookshelf app

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("relationship_app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("add_book/", include("relationship_app.urls")),
+    path("edit_book/", include("relationship_app.urls")),
+]
