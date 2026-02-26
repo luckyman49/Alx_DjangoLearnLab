ce1d6edd774ab5cb4a8ca68448e38c650b118694
ganiyu lukman <lganiyu43@gmail.com>
2026-02-26 18:38:36 +0100

    Fix AUTH_USER_MODEL to bookshelf.CustomUser

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "relationship_app",
+    "bookshelf",
 ]
-AUTH_USER_MODEL = "relationship_app.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
