f34a6659a03a4b033c4b75d9c99a817a1455991e
ganiyu lukman <lganiyu43@gmail.com>
2026-02-26 02:10:27 +0100

    Add path to fil

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,6 @@ from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     path("books/add/", views.add_book, name="add_book/"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book/"),
     path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
 ]
