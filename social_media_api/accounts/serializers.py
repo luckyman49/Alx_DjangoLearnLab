e613bb8e375462b3027a45973af0a8201e50ec71
ganiyu lukman <lganiyu43@gmail.com>
2026-03-04 04:48:40 +0100

    Fix Initial setu

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token, Token.objects.create, get_user_model().objects.create_use
+from rest_framework.authtoken.models import Token, rest_framework
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Explicitly use get_user_model().objects.create_user
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+
+
+
+
+
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -10,16 +10,30 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def feed(request):
-    followed_users = request.user.following.all()
-    
-    posts =Post.objects.filter(author__in=following_users).order_b
-    
-    posts = Post.objects.filter(author__in=followed_users).order_by ('-created_at')
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
