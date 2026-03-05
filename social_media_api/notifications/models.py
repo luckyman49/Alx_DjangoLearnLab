d3261b8863f274c44fe060413a412ba43df40ac5
ganiyu lukman <lganiyu43@gmail.com>
2026-03-05 23:06:23 +0100

    Add notification app

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,20 +1,16 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
+User = get_user_model()
+
 class Notification(models.Model):
-    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notifications')
-    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='actions')
-    verb = models.CharField(max_length=255)  # e.g. "liked your post"
-    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    target_object_id = models.PositiveIntegerField()
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="actions")
+    verb = models.CharField(max_length=255)
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    target_object_id = models.PositiveIntegerField(null=True)
     target = GenericForeignKey('target_content_type', 'target_object_id')
     timestamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.actor} {self.verb} {self.target} for {self.recipient}"
