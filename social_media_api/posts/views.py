0784daa6d0b9b0cf9032a463b27b4838b180e210
ganiyu lukman <lganiyu43@gmail.com>
2026-03-05 23:14:38 +0100

    Add notification ap

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,105 +1,32 @@
-from django.shortcuts import render
-
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-
+from rest_framework import status, generics
+from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
-
-
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
-
-
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
-
-
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from .models import Post, Like
 from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def like_post(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = generics.get_object_or_404(Post, pk=pk)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
     if created:
         Notification.objects.create(
             recipient=post.author,
             actor=request.user,
             verb="liked your post",
-            target=post,
-            target_content_type=ContentType.objects.get_for_model(Post),
-            target_object_id=post.id
+            target=post
         )
-        return Response({'message': 'Post liked'})
-    return Response({'message': 'Already liked'})
+        return Response({"message": "Post liked"}, status=status.HTTP_201_CREATED)
+    return Response({"message": "Already liked"}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def unlike_post(request, pk):
+    post = generics.get_object_or_404(Post, pk=pk)
     try:
-        like = Like.objects.get(user=request.user, post_id=pk)
+        like = Like.objects.get(user=request.user, post=post)
         like.delete()
-        return Response({'message': 'Post unliked'})
+        return Response({"message": "Post unliked"}, status=status.HTTP_200_OK)
     except Like.DoesNotExist:
-        return Response({'error': 'Not liked yet'})
-
-
-
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def feed(request):
-    following_users = request.user.following.all()
-    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
-
-
-
-
-
-
-
-
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        return Response({"message": "You haven't liked this post"}, status=status.HTTP_400_BAD_REQUEST)
